ffeb0d3b4133ae6001ed93ffbe6083636b22fa25
0du1chik <oleksandr.martynenko@student.sumdu.edu.ua>
2025-09-20 12:50:12 +0200

    Delete 1.3.py

--- 1.3.py
+++
@@ -1,8 +0,0 @@
-while True:
-    N = int(input("Введіть N (1 < N < 9): "))
-    if 1 < N < 9:
-        break
-for i in range(N, 0, -1):
-    for j in range(i, N + 1):
-        print(j, end=" ")
-    print()
